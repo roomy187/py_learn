7c4ebdf14a6cad74ef0412548289d02470a6fe6a
roomy187 <roomy187@gmail.com>
2020-11-16 17:12:08 +0100

    simple range use

---
+++ Game/game_looped.py
@@ -0,0 +1,32 @@
+# import & init random module
+
+import random
+random.seed()
+
+# random values & calculation
+
+a = random.randint(1,100)
+b = random.randint(1,100)
+c = a + b
+
+print("The task:", a, "+", b)
+
+# for loop (set number of tries)
+
+for i in 1, 2, 3, 4:
+
+    # input
+
+    print("your answer:")
+    x = input()
+    x = int(x)
+
+    # win condition
+    
+    if x == c:
+        
+        print("That's correct, Matherfucker!")
+        break
+
+    else:
+        print(x, "is wrong! guess again, you dumbfuck!")
---
+++ ranged_for_loop.py
@@ -0,0 +1,4 @@
+# for loop with range function
+
+for i in range(3,11,2):
+    print("Number:", i, "square:", i*i)
